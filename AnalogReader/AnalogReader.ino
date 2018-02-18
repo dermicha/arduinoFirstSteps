918a03b82b440e906ba08f7c6e1e1c4f765de2d0
derMicha <dermicha@dermicha.de>
2018-02-18 21:26:22 +0100

    added some source code docs

--- AnalogReader/AnalogReader.ino
+++ AnalogReader/AnalogReader.ino
@@ -1,10 +1,17 @@
-int analogPin = 5;     // potentiometer wiper (middle terminal) connected to analog pin 3
+/*
+ * AnalogReader
+ * Reads from analog pin and writes result to serial. 
+ * 
+ * Use plotter from Arduino IDE for a nice visualization.
+ */
+
+int analogPin = 5;     // potentiometer wiper (middle terminal) connected to analog pin 5
                        // outside leads to ground and +5V
 int val = 0;           // variable to store the value read
 
 void setup()
 {
-  Serial.begin(9600);              //  setup serial
+  Serial.begin(9600);              //  setup serial, 9600 is a good default
 }
 
 void loop()
